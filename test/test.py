aba41c39902f5822179061e6153c204c3d7e2e98
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-31 18:51:06 +0200

    Primo test progetto zoo

--- test/test.py
+++ test/test.py
@@ -4,7 +4,28 @@ from src.zoo import Zoo, ZooKeeper, Animal, Fence
 
 class TestZoo(TestCase):
     
-    def setUp(self):
+    def setUp(self) -> None:
         
-        pass
+        self.zoo_1: Zoo = Zoo()
+        self.zookeeper_1: ZooKeeper = ZooKeeper(name="Gianni", surname="Rossi", id=1)
+        self.fence_1: Fence = Fence(area=100, temperatura=25.0 habitat="Savana")
+        self.animal_1: Animal = Animal(name="Pluto", species="Canide", age=5, height=300.0, width=1.0, preferred_habitat="Savana")
+        
+    def test_animal_dimensioni(self):
+            """
+            
+            Questo Test controlla che animali troppon grandi non vengano aggiunti alla fence
+            
+            """
+            
+            self.zookeeper_1.add_animal(self.animal_1, self.fence_1)
+            result: int = len(self.fence_1.list_of_animals)
+            message: str = f"Error: the function add_animal should not add self.animal_1 into self.fence_1"
+            
+    if __name__ == "__main__":
+            
+        unittest.main()
+            
+        
+    
         
