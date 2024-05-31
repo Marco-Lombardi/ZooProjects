4d75619eaf520adbc6c804d12ece10970d6d36cf
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-31 18:16:40 +0200

    Import librerie

---
+++ test/test.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest import TestCase
+from src.zoo import Zoo, ZooKeeper, Animal, Fence
+
+class TestZoo(TestCase):
+    
+    def setUp(self):
+        
+        pass
+        
